f6fd6747c673e47f2d8c0706994acd27ff6a8e6a
MaxiCappe256 <maxicappe256@gmail.com>
2026-04-02 20:12:26 -0300

    finished portfolio

--- js/js.js
+++ js/js.js
@@ -3,17 +3,39 @@ const menu = document.querySelector('.navbar__links');
 const links = document.querySelectorAll('.navbar__link');
 
 hamburguer.addEventListener('click', () => {
-    menu.classList.toggle('active');
-})
+  menu.classList.toggle('active');
+});
 
-links.forEach(link => {
-    link.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    })
-})
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  });
+});
 
 document.addEventListener('click', (e) => {
-    if(!menu.contains(e.target) && !hamburguer.contains(e.target)) {
-        menu.classList.remove('active');
-    }
+  if (!menu.contains(e.target) && !hamburguer.contains(e.target)) {
+    menu.classList.remove('active');
+  }
+});
+
+const btn = document.getElementById('button');
+
+document.getElementById('form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  btn.value = 'Sending...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_gjni9er';
+
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    },
+    (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    },
+  );
 });
